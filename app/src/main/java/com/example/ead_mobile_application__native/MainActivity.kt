9a7c21baecd73bed175566ea9c41ee9b0c196848
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-22 21:28:01 +0530

    navigation bar and app icon

--- app/src/main/java/com/example/ead_mobile_application__native/MainActivity.kt
+++ app/src/main/java/com/example/ead_mobile_application__native/MainActivity.kt
@@ -2,20 +2,11 @@ package com.example.ead_mobile_application__native
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.ead_mobile_application__native.databinding.ActivityMainBinding
 import com.example.ead_mobile_application__native.screen.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -25,16 +16,5 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
         finish()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
-        }
     }
 }
